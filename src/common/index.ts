5072338949240e9fe823b89e2ef536df1b30d36d
steveschult <steveschultz8864@gmail.com>
2024-12-05 23:20:47 +0800

    feat: login btc

---
+++ src/common/index.ts
@@ -0,0 +1,27 @@
+export function getParameterByName(name: string, url: string): string | null {
+  if (!url) url = window.location.href
+  name = name.replace(/[[\]]/g, '\\$&')
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  const results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
+}
+
+export function isJwtExpired(exp: number): boolean {
+  const expirationTime = exp * 1000;
+  const currentTime = Date.now();
+
+  return currentTime < expirationTime;
+}
+
+export type Nullable<T> = T | null
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export function getValidInterval(interval: number | string) {
+  const _interval = Number(interval);
+  return isNaN(_interval) ? 10 : Math.max(_interval || 0, 10);
+}
--- src/components/views/homeBtns.tsx
+++ src/components/views/homeBtns.tsx
@@ -1,21 +1,55 @@
 "use client";
 
-import { googleAuthUrl } from "@/common/constants";
+import { googleAuthUrl, TOKEN_FIELD } from "@/common/constants";
 import { Button } from "../ui/button";
+import Cookies from 'js-cookie';
+import { isJwtExpired } from "@/common";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export function HomeBtns() {
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const token = Cookies.get(TOKEN_FIELD);
+    const exp = Cookies.get('exp');
+    if (token && exp) {
+      setIsAuthenticated(isJwtExpired(Number(exp)));
+    }
+    setIsLoading(false);
+  }, []);
+
   const onLoginWGoogle = () => {
-    window.open(googleAuthUrl)
+    window.location.href = googleAuthUrl;
+  };
+
+  const onLaunch = () => {
+    router.push('/dashboard');
+  };
+
+  if (isLoading) {
+    return null
   }
 
   return (
     <div className="flex gap-4">
-      <Button 
-        onClick={() => onLoginWGoogle()}
-        className="bg-blue-500 hover:bg-blue-600 text-white dark:bg-blue-600 dark:hover:bg-blue-700 transition-colors duration-200"
-      >
-        Login with Google
-      </Button>
+      {isAuthenticated ? (
+        <Button
+          onClick={onLaunch}
+          className="bg-blue-500 hover:bg-blue-600 text-white dark:bg-blue-600 dark:hover:bg-blue-700 transition-colors duration-200"
+        >
+          Launch App
+        </Button>
+      ) : (
+        <Button
+          onClick={onLoginWGoogle}
+          className="bg-blue-500 hover:bg-blue-600 text-white dark:bg-blue-600 dark:hover:bg-blue-700 transition-colors duration-200"
+        >
+          Login with Google
+        </Button>
+      )}
     </div>
-  )
+  );
 }
